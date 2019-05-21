867d7ecf34f8226aeb93d17e02218c9fce7f65de
marinacha <48958470+marinacha@users.noreply.github.com>
2019-05-21 17:36:03 +0200

    UPDATE getInfo() pour questions en 2 temps

--- MEMORY.py
+++ MEMORY.py
@@ -1,6 +1,5 @@
-
-
 class Memory(object):
+    """Use to create a memory record of the conversation"""
 
     def __init__(self, question):
         self.question = question
@@ -10,6 +9,12 @@ class Memory(object):
 
 
     def getLast(self):
+        """Use to get the last answer given by the chatbot"""
 
         memoData = self.answer[-1]
+        return memoData
+
+    def getInfo(self):
+
+        memoData = self.info[-1]
         return memoData
